b17be04327900be002b158787836e031d0a24e1b
Mainak Basak <108523198+mossfit@users.noreply.github.com>
2025-03-23 03:54:55 +0900

    final commit

--- spc_project/settings.py
+++ spc_project/settings.py
@@ -1,3 +1,13 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="YOUR_SENTRY_DSN_HERE",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
